bbe7bdbb41f7c1a3acbec47bc5b80b87647727cf
simran-afk <simran@citicollege.ca>
2026-01-30 20:16:02 -0500

    Create commands.php with Git commands
    Add Git commands and usage instructions to commands.php

---
+++ friday/commands.php
@@ -0,0 +1,12 @@
+<?php 
+echo "Git Commands"; 
+?>
+This is a readme File
+cd folder-name/
+dir : list all dir/files in the current path
+git --version
+git branch demo-feature
+git add .
+echo "Demo" > demo.txt
+git remote add origin <URL>
+git push -u origin demo-feature
